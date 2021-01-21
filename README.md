a0d90e5368681527446fcca99066fae4499b229b
Jean-Luc Auge <jeanluc.auge@orange.com>
2021-01-21 14:31:36 +0100

    full version
    Signed-off-by: Jean-Luc Auge <jeanluc.auge@orange.com>

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
-#FLASK DECORATED with meta programming
+# FLASK DECORATED with meta programming
 
 - automates the creation of Flask endpoints
 - alternative to jinja templating
 
-**This is the manual simple version**
- 
-automates the creation of Flask endpoints in a dynamic way with meta programming:
+**This is the full version with code inspection**
+
+With the following modules:
+- Flask server with meta programming in flask_restplus_server.py 
+- The macros functions are implemented in macros.py
+- The definition of endpoints and macros to expose in cep.py
+- The code inspection to build the data model in generate_endpoints.py
+
+Automates the creation of Flask endpoints in a dynamic way with meta programming:
+- build data model with code inspection
 - dynamic class creation with *type*
 - dynamic decoration of class with Flask decorators
 
+## use
+pip3 install -rrequirements.txt
+python3 python3 flask_restplus_server.py
+
+You can add or remove macros to expose in cep.py
--- flask_restplus_server.py
+++ flask_restplus_server.py
@@ -19,59 +19,59 @@ secret = api.namespace("secret", description="macros for Akamai LCDN secret")
 secret_client = LcdnSecretClient()
 
 
-# def get_rest_method(client, func_name):
-#     def wrapper(self, *args, **kwargs):
-#         response = getattr(client, func_name)(*args, **kwargs)
-#         status_code = 200 if response else 400
-#         return (response, status_code)
-#
-#     return wrapper
-#
-#
-# reduce_attr = lambda base_object, attr_list: functools.reduce(
-#     getattr, attr_list, base_object
-# )
+def get_rest_method(client, func_name):
+    def wrapper(self, *args, **kwargs):
+        response = getattr(client, func_name)(*args, **kwargs)
+        status_code = 200 if response else 400
+        return (response, status_code)
 
-# for data in render_data_generator():
-#     # {"endpoints": endpoints, "namespace": namespace, "client": client}
-#     # endpoints = {
-#     #     endpoint_name: {
-#     #         'class_name': class_name,
-#     #         'methods': [{
-#     #             'rest_method': func.rest_method,
-#     #             'func_name': func.__name__,
-#     #             'func_args': {'path_args': _, 'free_args': _}
-#     #         }]
-#     #     }
-#     # }
-#     module = sys.modules[__name__]
-#     client = getattr(module, data.client)
-#     namespace = getattr(module, data.namespace)
-#     for endpoint_name, endpoint_data in data.endpoints.items():
-#         endpoint_class = type(
-#             endpoint_data.class_name,
-#             (Resource,),
-#             {
-#                 method.rest_method: api.doc(description=reduce_attr(module, [data.client, method.func_name, '__doc__']))(
-#                     get_rest_method(client,method.func_name)
-#                 )
-#                 for method in endpoint_data.methods
-#             }
-#         )
-#         endpoint_class = getattr(namespace, 'route')(endpoint_name)(endpoint_class)
+    return wrapper
 
 
-endpoint_class = type(
-    "Provider_account",
-    (Resource,),
-    {
-        "get": api.doc(description=contentd_client.get_content_provider.__doc__)(
-            lambda account: contentd_client.get_content_provider(account)
-        )
-    },
+reduce_attr = lambda base_object, attr_list: functools.reduce(
+    getattr, attr_list, base_object
 )
 
-endpoint_class = contentd.route("/provider/<string:account>")(endpoint_class)
+for data in render_data_generator():
+    # {"endpoints": endpoints, "namespace": namespace, "client": client}
+    # endpoints = {
+    #     endpoint_name: {
+    #         'class_name': class_name,
+    #         'methods': [{
+    #             'rest_method': func.rest_method,
+    #             'func_name': func.__name__,
+    #             'func_args': {'path_args': _, 'free_args': _}
+    #         }]
+    #     }
+    # }
+    module = sys.modules[__name__]
+    client = getattr(module, data.client)
+    namespace = getattr(module, data.namespace)
+    for endpoint_name, endpoint_data in data.endpoints.items():
+        endpoint_class = type(
+            endpoint_data.class_name,
+            (Resource,),
+            {
+                method.rest_method: api.doc(description=reduce_attr(module, [data.client, method.func_name, '__doc__']))(
+                    get_rest_method(client,method.func_name)
+                )
+                for method in endpoint_data.methods
+            }
+        )
+        endpoint_class = getattr(namespace, 'route')(endpoint_name)(endpoint_class)
+
+
+# endpoint_class = type(
+#     "Provider_account",
+#     (Resource,),
+#     {
+#         "get": api.doc(description=contentd_client.get_content_provider.__doc__)(
+#             lambda account: contentd_client.get_content_provider(account)
+#         )
+#     },
+# )
+#
+# endpoint_class = contentd.route("/provider/<string:account>")(endpoint_class)
 
 # ********** endpoints declaration ****************
 # @contentd.route('/provider/<string:account>')
